4def28fa1c1951730d19c933d89eb2f76eeba704
Rohit Chandra <chandrarohit91@gmail.com>
2021-02-18 18:37:11 +0530

    basic of MaterialApp Widget

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(QuizeApp());
+
+class QuizeApp extends StatelessWidget {
+  const QuizeApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text("Hello World"),
+    );
+  }
+}
